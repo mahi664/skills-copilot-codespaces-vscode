f716d8fe43715887624cd93c8a21807cb40cc705
Mahesh Ghuge <mahighuge664@gmail.com>
2024-05-03 18:14:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMembers() {
+  return {
+    1: {
+      name: 'Talha',
+      job: 'Frontend Developer',
+    },
+    2: {
+      name: 'Mehmet',
+      job: 'Backend Developer',
+    },
+    3: {
+      name: 'Hakan',
+      job: 'Full Stack Developer',
+    },
+  };
+}
